bed926bf0e9bdcc67ac48cec5d624c1d548f8153
pshenichque <pshenichque@mail.ru>
2023-07-26 20:46:39 +0300

    Create note template and pre-rendered notes

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
-const inputElement = document.getElementById('input')
-const createButton = document.getElementById('create')
-const listElement = document.getElementById('list')
+const inputElement = document.getElementById('input');
+const createButton = document.getElementById('create');
+const listElement = document.getElementById('list');
+
+const notes = ['помыть кошк', 'почесать кошк'];
+
+const getNoteTemplate = (title) => {
+    return  `
+        <li class="list-item">
+        <span>${title}</span>
+            <div>
+                <button class="button-ok">Выполнено</button>
+                <button class="button-ok red">Удалить</button>
+            </div>
+        </li>
+    `
+};
+
+const render = () => {
+    listElement.insertAdjacentHTML('afterbegin', getNoteTemplate(notes[1]));
+    listElement.insertAdjacentHTML('afterbegin', getNoteTemplate(notes[0]));
+};
+
+render();
 
 createButton.onclick = () => {
     if (inputElement.value.length === 0) {
         return
     }
     listElement.insertAdjacentHTML(
-        'afterbegin', 
-        `
-            <li class="list-item">
-            <span>${inputElement.value}</span>
-                <div>
-                    <button class="button-ok">Выполнено</button>
-                    <button class="button-ok red">Удалить</button>
-                </div>
-            </li>
-        `
+        'afterbegin',
+        getNoteTemplate(inputElement.value)
     );
     inputElement.value = '';
 };
+
+
